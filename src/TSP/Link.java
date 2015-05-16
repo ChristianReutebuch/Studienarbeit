ad2acaa7e28acccfbcd38feb1819c5a5240e5be6
ChristianReutebuch <chris.r93@gmx.de>
2015-05-16 09:21:09 +0200

    Show Distance at link
    Die Distanzen zwischen den Knoten werden im Graphen jetzt angezeigt.
    Standardmässig stehen die noch alle auf eins.

--- src/TSP/Link.java
+++ src/TSP/Link.java
@@ -7,6 +7,8 @@ public class Link {
 	private Node firstNode;
 	private Node secondNode;
 	private int distance;
+	private float xPosLbl;
+	private float yPosLbl;
 	
 	public Link(Node firstNode, Node secondNode, int distance){
 		this.firstNode = firstNode;
@@ -17,6 +19,9 @@ public class Link {
 	public void paintLink(Graphics g) {
 		g.setColor(Color.BLACK);
 		g.drawLine(firstNode.getXPos()+20, firstNode.getYPos()+20, secondNode.getXPos()+20, secondNode.getYPos()+20);
+		String text = Integer.toString(distance);
+		calcPosLbl();
+		g.drawString(text, (int)xPosLbl, (int)yPosLbl);
 	}
 	
 	public Node getFirstNode() {
@@ -29,5 +34,22 @@ public class Link {
 	
 	public int getDistance() {
 		return distance;
+	}
+	
+	public void calcPosLbl(){
+		float xAbstand = (Math.abs(firstNode.getXPos() - secondNode.getXPos()))/2;
+		float yAbstand = (Math.abs(firstNode.getYPos() - secondNode.getYPos()))/2;
+		if ( firstNode.getXPos() <= secondNode.getXPos()){
+			xPosLbl = firstNode.getXPos() + xAbstand;
+		}
+		else{
+			xPosLbl = secondNode.getXPos() + xAbstand;
+		}
+		if ( firstNode.getYPos() <= secondNode.getYPos()){
+			yPosLbl = firstNode.getYPos() + yAbstand;
+		}
+		else{
+			yPosLbl = secondNode.getYPos() + yAbstand;
+		}
 	}
 }
