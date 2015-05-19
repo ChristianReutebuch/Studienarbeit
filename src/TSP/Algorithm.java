2825ad3d6fd011bef26402f607fe7c208be19353
si183 <sh@h-mails.de>
2015-05-19 15:27:15 +0200

    Kommentare

--- src/TSP/Algorithm.java
+++ src/TSP/Algorithm.java
@@ -7,10 +7,14 @@ public class Algorithm {
 	private static ArrayList<Integer> bestRoute;
 	
 	public static void bruteForce(ArrayList<Integer> route, ArrayList<Integer> NodesNotInRoute){
+		//prüfen, ob die übergebene Knotenliste Knoten enthält
 		if(!NodesNotInRoute.isEmpty()){
+			//Schleife über Knoten
 			for(int i = 0; i<NodesNotInRoute.size();i++){
 				Integer justRemoved = (Integer) NodesNotInRoute.remove(0);
+				//übergebene Liste kopieren
 				ArrayList<Integer> newRoute = (ArrayList<Integer>) route.clone();
+				//erstes, zuvor gelöschtes Element hinten anhängen
 				newRoute.add(justRemoved);
 				bruteForce(newRoute, NodesNotInRoute);
 				NodesNotInRoute.add(justRemoved);
